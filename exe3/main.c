284d6b7e6e92bc9b83a1b0eaee3aadf827f84045
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2025-04-14 01:38:07 -0300

    exe3

--- exe3/main.c
+++ exe3/main.c
@@ -11,6 +11,7 @@
 #include "mpu6050.h"
 
 const int I2C_CHIP_ADDRESS = 0x68;
+const int BMP280_CHIP_ADDRESS = 0x76;
 const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
 
@@ -23,7 +24,11 @@ void i2c_task(void *p) {
 
     // TODO
     // read id chip BMP280
-    // printf("BMP280 ID: 0x%X \n", buffer[0]);
+    uint8_t buffer[1];
+    uint8_t reg_address = 0xD0; // BMP280 chip ID register
+    i2c_write_blocking(i2c_default, BMP280_CHIP_ADDRESS, &reg_address, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, BMP280_CHIP_ADDRESS, buffer, 1, false);
+    printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
